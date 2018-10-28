f1bed06c344896eb238678cef3a97c5f1f94f747
kylhansen <kylhansen@westmont.edu>
2018-10-27 23:03:42 -0700

    completed survey data collection

--- survey.py
+++ survey.py
@@ -25,6 +25,7 @@ def survey_q0():
         session['email'] = email
         return redirect(url_for('survey.survey_q1'))
 
+    # Render demographic/personal information question
     return render_template('survey_q0.html')
 
 @survey.route('/survey_q1', methods=["POST", "GET"])
@@ -38,12 +39,17 @@ def survey_q1():
         # Fetch the categories which the user selected and put them in a list
         categories = list()
         for key in request.form.keys():
+            print(key)
+            print(request.form[key])
             if request.form[key]:
                 categories.append(key)
-        # Save categories in a sesson
+        # Save all categories selected
         session['categories'] = categories
+
+        # Send the user to the next question
         return redirect(url_for('survey.survey_q2'))
-    # Render survey_q1
+
+    # Render question 1
     return render_template('survey_q1.html', professional = professional_categories, personal = personal_categories)
 
 @survey.route('/survey_q2', methods=["POST", "GET"])
@@ -53,16 +59,51 @@ def survey_q2():
         # Fetch the categories which were selected and put them in a list
         for value in request.form.values():
             categories.append(value)
+
+        # Save top categories selected
         session['top_categories'] = categories
+
+        # Send the user to the next question
         return redirect(url_for('survey.survey_q3'))
+
+    # Render question 2
     return render_template('survey_q2.html', selected_categories=session['categories'])    
 
 @survey.route('/survey_q3', methods=["POST", "GET"])
 def survey_q3():
     if request.method == "POST":
-        return render_template('survey_q4.html')
+        # Fetch the categories which the user selected and put them in a list
+        categories = list()
+        for key in request.form.keys():
+            if request.form[key]:
+                categories.append(key)
+
+        # Save categories for which they don't know everything they need
+        session['question_categories'] = categories
+
+        # Send the user to the next question
+        return redirect(url_for('survey.survey_q4'))
+
+    # Render question 3
     return render_template('survey_q3.html', top_categories=session['top_categories'])
 
+@survey.route('/survey_q4', methods=["POST", "GET"])
+def survey_q4():
+    if request.method=="POST":
+        q_and_a = dict()
+        # Fetch the question/answer pair for the response
+        for key,value in request.form.items():
+            print(key)
+            print(value)
+            q_and_a[key] = value
+        # TODO: pass all relevant information from the session off to the database
+        session['q_and_a'] = q_and_a
+
+        # Send the user to the "thank you" page
+        return render_template("survey_end.html")
+
+    # Render question 4
+    return render_template("survey_q4.html", question_categories=session['question_categories'])
 
 
 
