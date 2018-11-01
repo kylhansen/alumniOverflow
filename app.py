6c14eea09ec2b2e0a653767e340d2e7959d9e0f9
trevorKirkby <trevor.p.kirkby@gmail.com>
2018-10-31 17:14:18 -0700

    Blueprint for question view created.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 #from alumniOverflow.survey_q4 import survey_q4
 from survey import survey
+from question_view import question_view
 
 app = Flask(__name__)
 app.secret_key = 'alumniOverflow'
 
 #app.register_blueprint(survey_q4)
 app.register_blueprint(survey)
+app.register_blueprint(question_view)
 
 @app.route('/')
 def hello():
