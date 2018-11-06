683bbab457ad683fdf20d3e2f9e3ccd2a84916e6
kylhansen <kylhansen@westmont.edu>
2018-11-06 08:48:32 -0800

    Filled out the page for listing questions by their category.

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, url_for
 from survey import survey
-from search import search
+from list_questions import list_questions
 
 app = Flask(__name__)
 app.secret_key = 'alumniOverflow'
 app.url_map.strict_slashes = False
 
 app.register_blueprint(survey)
-app.register_blueprint(search)
+app.register_blueprint(list_questions)
 
 @app.route('/')
 def home():
-    return "<a href=" + url_for('survey.survey_q0') + ">Link to survey</a><br><a href=" + url_for('search.search_questions')  + ">Link to view questions</a>"
+    return "<a href=" + url_for('survey.survey_q0') + ">Link to survey</a><br><a href="+ url_for('list_questions.display')  + ">Link to view questions</a>" 
 
 if __name__ == '__main__':
     app.run()
