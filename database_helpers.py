1d12c999b972794efa3db8dda4ecc6c835b8a42e
Mckalie Drown <mckaliedrown@Mckalies-MacBook-Air.local>
2018-12-05 21:03:06 -0800

    added delete answer function

--- database_helpers.py
+++ database_helpers.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask import redirect, url_for
 
 def get_question(questionid):
     connection = sqlite3.connect('database/2468')
@@ -9,3 +10,11 @@ def get_answers(questionid):
     connection = sqlite3.connect('database/2468')
     cursor = connection.cursor()
     return cursor.execute('SELECT * FROM RespondsTo WHERE id = ?', [questionid]).fetchall()
+
+def deleteAnswer(id, answer_text, user):
+    connection = sqlite3.connect('database/2468')
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM RespondsTo WHERE id = ? AND answer = ?", [id, answer_text])
+    connection.commit()
+    connection.close()
+    return redirect(url_for('question_view.view_question', questionid=id, user=user))
