6424aa8a95627ad2a46ab31e3c6448ece5a8873b
mrouillardv <42824735+mrouillardv@users.noreply.github.com>
2018-12-12 12:09:18 -0800

    Deleted the recursive alumniOverflows

--- survey.py
+++ survey.py
@@ -1,7 +1,7 @@
 #coding: utf8
-# 
-# Serves a page that laets a client answer the initial survey 
-# 
+#
+# Serves a page that laets a client answer the initial survey
+#
 
 import sqlite3
 from flask import Blueprint, session, render_template, request, redirect, url_for
@@ -9,10 +9,10 @@ from flask import Blueprint, session, render_template, request, redirect, url_fo
 survey = Blueprint('survey', __name__, template_folder='templates')
 
 @survey.route('/survey_q0', methods=["POST", "GET"])
-def survey_q0():
+def survey_q0(valid_input=True):
     # Collect responses on personal information
     if request.method=="POST":
-        # TODO: if needed, put in database here. 
+        # TODO: if needed, put in database here.
         #   Otherwise, wait to add all data at the end, and save all this in the session (Probably the latter)
         conn = sqlite3.connect('database/2468')
         c = conn.cursor()
@@ -29,11 +29,11 @@ def survey_q0():
         if c.fetchone() != None:
             c.close()
             conn.close()
-            return redirect(url_for('survey.survey_q0'))
+            return redirect(url_for('survey.survey_q0', valid_input=False))
 
         c.close()
         conn.close()
-        # Add all keys to the session with the user input data 
+        # Add all keys to the session with the user input data
         session['first_name'] = first_name
         session['last_name'] = last_name
         session['email'] = email
@@ -42,8 +42,8 @@ def survey_q0():
         session['grad_year'] = grad_year
         session['majors'] = majors
 
-        
-        return redirect(url_for('survey.survey_q1'))
+
+        return redirect(url_for('survey.survey_q1', valid_input=True))
 
     # Render demographic/personal information question
     return render_template('survey_q0.html')
@@ -84,7 +84,7 @@ def survey_q2():
         return redirect(url_for('survey.survey_q3'))
 
     # Render question 2
-    return render_template('survey_q2.html', selected_categories=session['categories'])    
+    return render_template('survey_q2.html', selected_categories=session['categories'])
 
 @survey.route('/survey_q3', methods=["POST", "GET"])
 def survey_q3():
@@ -113,13 +113,13 @@ def survey_q4():
             c_and_q[category] = question
         # TODO: pass all relevant information from the session off to the database
         session['c_and_q'] = c_and_q
-        
+
         #loads all data into database
         load_db()
 
         # Send the user to the "thank you" page
         return render_template("survey_end.html")
-    
+
     # Render question 4
     return render_template("survey_q4.html", question_categories=session['question_categories'])
 
@@ -131,7 +131,7 @@ def get_categories():
     categories = set()
     for row in c.execute('SELECT category FROM categories'):
         categories.add(row[0])
-        
+
     c.close()
     conn.close()
     return categories
@@ -141,35 +141,35 @@ def load_db():
     #opens connection with the local database, and creates a cursor that allows commands to be sent to the database
     conn = sqlite3.connect('database/2468')
     c = conn.cursor()
-   
+
 
     #list of data to be inserted into participants table
     insert_list_par = [(str(session['first_name']), str(session['last_name']), str(session['email']),
                          str(session['dob']), str(session['institution']), str(session['grad_year']),
                          str(session['majors']))]
-        
+
     #saves command that inserts user input into the Participants table of the local database to be excuted later at end of function call
     c.executemany('INSERT INTO Participants VALUES (?,?,?,?,?,?,?)', insert_list_par)
-    
-    
+
+
     #inserts data into question one table
     for cat in session['categories']:
-       insert_list_q1 = [session['email'], cat] 
+       insert_list_q1 = [session['email'], cat]
        c.execute('INSERT INTO QuestionOne VALUES (?,?)', insert_list_q1)
        insert_list_q1 = list()
-       
+
     #inserts data into question two table
     for cat in session['top_categories']:
-       insert_list_q2 = [session['email'], cat] 
+       insert_list_q2 = [session['email'], cat]
        c.execute('INSERT INTO QuestionTwo VALUES (?,?)', insert_list_q2)
        insert_list_q2 = list()
-       
+
     #inserts data into question three table
     for cat in session['question_categories']:
-       insert_list_q3 = [session['email'], cat] 
+       insert_list_q3 = [session['email'], cat]
        c.execute('INSERT INTO QuestionThree VALUES (?,?)', insert_list_q3)
        insert_list_q3 = list()
-       
+
 
     #inserts data into question 4 table
     insert_list_q4 = []
@@ -183,12 +183,3 @@ def load_db():
     conn.commit()
     c.close()
     conn.close()
-
-
-
-
-
-
-
-
-
